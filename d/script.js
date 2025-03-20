6be78a21c92126f184a171f4fac2a2b45321de33
daj3on9 <dajung5623@naver.com>
2025-03-20 17:29:10 +0900

    Feat : 검색 기능 추가

--- d/script.js
+++ d/script.js
@@ -1,1 +1,11 @@
-let a;
+// 검색어 입력 후 버튼 클릭시 검색 결과로 이동
+document
+  .getElementById("google-search-btn")
+  .addEventListener("click", function () {
+    const query = document.querySelector(".search-input").value;
+    if (query) {
+      document.querySelector(".search-form").submit();
+    } else {
+      alert("검색어를 입력해주세요!");
+    }
+  });
